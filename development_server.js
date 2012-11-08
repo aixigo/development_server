6aedf36b53b74aa06b26f30c139252fd58a6ea42
Alexander Wilden <alexander.wilden@aixigo.de>
2012-11-08 14:14:31 +0100

    refactorings

--- development_server.js
+++ development_server.js
@@ -13,23 +13,50 @@
 //  Aachen, Germany
 //
 var express = require( 'express' );
+var optimist = require( 'optimist' );
 var static_server = require( './static_server' );
+var directory_tree_provider = require( './directory_tree_provider' );
 
+
+var argv = optimist
+   .usage( 'Starts a development web server.\n' +
+      'Usage: $0 --web-dir=<path>\n' +
+      '       The export-dir argument can be given multiple times for different directories' )
+   .demand( [ 'web-dir' ] )
+   .describe( 'web-dir', 'The directory to serve' )
+   .describe( 'export-dir', 'A directory to serve as http://localhost:<port>/var/listing/<directory>' )
+   .describe( 'port', 'The port to start the server with' )
+   .default( { port: 8666 } )
+   .argv;
 var app = express();
-
-console.log( 'Using root directory %s', process.argv[ 2 ] );
-
-if( process.argv.length < 3 ) {
-   printHelp();
-   process.exit();
+var port = argv.port;
+var rootDir = argv['web-dir'].replace( /\/$/, '' );
+var exportDirs = [];
+if( argv[ 'export-dir' ] ) {
+   if( typeof argv[ 'export-dir' ] === 'string' ) {
+      exportDirs = [ argv[ 'export-dir' ] ];
+   }
+   else {
+      // assume it is an array provided by optimist
+      exportDirs = argv[ 'export-dir' ];
+   }
 }
-static_server.start( app, process.argv[ 2 ] );
 
-app.listen( 8666 );
+console.log( argv );
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+console.log( 'Started with pid ' + process.pid );
+console.log( 'Using root directory %s and port %s', rootDir, port );
 
-function printHelp() {
-   console.log( 'Usage: node development_server.js <path>' );
-   console.log( ' where path is the root of the product' );
-}
+static_server.start( app, rootDir );
+
+exportDirs.forEach( function( dir ) {
+   dir = dir.replace( /^\//, '' );
+   var widgetDirectoryTreeProvider = directory_tree_provider.getInstance( rootDir, dir );
+   app.get( '/var/listing/' + dir.replace( /\//g, '_' ) + '.json', function( req, res ) {
+      res.json( widgetDirectoryTreeProvider.getTree() );
+   } );
+} );
+
+app.listen( port );
---
+++ directory_tree_provider.js
@@ -0,0 +1,108 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2000-2012
+//    by aixigo AG, Aachen, Germany.
+//
+//  All rights reserved. This material is confidential and proprietary to AIXIGO AG and no part of this
+//  material should be reproduced, published in any form by any means, electronic or mechanical including
+//  photocopy or any information storage or retrieval system nor should the material be disclosed to third
+//  parties without the express written authorization of AIXIGO AG.
+//
+//  aixigo AG
+//  http://www.aixigo.de
+//  Aachen, Germany
+//
+'use strict';
+
+var Q = require( 'q' );
+var fs = require( 'fs' );
+var fsWatchTree = require( 'fs-watch-tree' );
+
+exports.getInstance = getInstance;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getInstance( rootDirectory, subDirectory ) {
+   var watchedDirectory = rootDirectory + '/' + subDirectory;
+
+   var fileTree = [];
+   var currTree = fileTree;
+   subDirectory.split( '/' ).forEach( function( dir ) {
+      currTree.push( {
+         ignored: true,
+         name: dir,
+         children: []
+      } );
+      currTree = currTree[ 0 ].children;
+   } );
+
+   function updateFileTreeCache() {
+      currTree.splice( 0, currTree.length );
+      buildTree( watchedDirectory ).then( function( tree ) {
+         tree.forEach( function( item ) {
+            currTree.push( item );
+         } );
+      } );
+   }
+   updateFileTreeCache();
+
+   fsWatchTree.watchTree( watchedDirectory, updateFileTreeCache );
+
+   return {
+      getTree: function() {
+         return fileTree;
+      }
+   };
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function buildTree( path ) {
+   var deferred = Q.defer();
+   var tree = [];
+
+   fs.readdir( path, function( err, files ) {
+      if( err ) {
+         deferred.reject( err );
+         return;
+      }
+
+      var promises = [];
+      files.forEach( function( dir ) {
+         var fullPath = path + '/' + dir;
+         var dirDeferred = Q.defer();
+         promises.push( dirDeferred.promise );
+         fs.stat( fullPath, function( err, stat ) {
+            if( err ) {
+               dirDeferred.reject( err );
+               return;
+            }
+
+            if( stat.isDirectory() ) {
+               buildTree( fullPath ).then( function( subDir ) {
+                  var subtree = {
+                     name: dir,
+                     children: subDir
+                  };
+                  tree.push( subtree );
+                  dirDeferred.resolve();
+               } );
+            }
+            else if( stat.isFile() ) {
+               tree.push( dir );
+               dirDeferred.resolve();
+            }
+            else {
+               // something else that we just ignore
+               dirDeferred.resolve();
+            }
+         } );
+      } );
+
+      Q.all( promises ).then( function() {
+         deferred.resolve( tree );
+      } );
+   } );
+
+   return deferred.promise;
+}
